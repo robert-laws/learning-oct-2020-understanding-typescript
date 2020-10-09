7c1ca9474aaf3ad7702785e606390fe9d41e2c72
Robert Laws <roblaws@me.com>
2020-10-09 18:10:33 +0300

    begin topic types

--- app.ts
+++ app.ts
@@ -1,1 +1,12 @@
-console.log('hello, there typescript...');
+// types number, string, boolean
+function add(n1: number, n2: number) {
+  console.log(typeof n1);
+  return n1 + n2;
+}
+
+const number1 = 4;
+const number2 = 5;
+
+const result = add(number1, number2);
+
+console.log(result);
