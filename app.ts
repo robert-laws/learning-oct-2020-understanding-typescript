a8bd90541510eea86085c720d51ca490d4dc5752
Robert Laws <roblaws@me.com>
2020-10-10 11:05:02 +0300

    finish basic types

--- app.ts
+++ app.ts
@@ -1,12 +1,23 @@
 // types number, string, boolean
-function add(n1: number, n2: number) {
-  console.log(typeof n1);
-  return n1 + n2;
+function add(n1: number, n2: number, showResult: boolean) {
+  if (showResult) {
+    console.log(n1 + n2);
+  } else {
+    return n1 + n2;
+  }
 }
 
-const number1 = 4;
+const subtract = (num1: number, num2: number): number => {
+  return num1 - num2;
+};
+
+let myValue: number;
+let number1 = 4;
 const number2 = 5;
+const printResult = true;
 
-const result = add(number1, number2);
+number1 = 8;
 
-console.log(result);
+const result = add(number1, number2, printResult);
+
+console.log(subtract(34, 21));
