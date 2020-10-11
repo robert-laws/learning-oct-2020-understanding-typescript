2d108b0a70eede0861b86893100a2a5c1fdc18a6
Robert Laws <roblaws@me.com>
2020-10-11 20:08:57 +0300

    begin interfaces

--- src/app.ts
+++ src/app.ts
@@ -1,98 +1,33 @@
-abstract class Department {
-  // private readonly id: string;
-  // private name: string;
-  private workers: string[] = [];
-  static fiscalYear = 2020;
+interface Greetable {
+  _name: string;
 
-  constructor(protected readonly id: number, protected name: string) {
-    // this.id = id;
-    // this.name = name;
-  }
-
-  static createEmployee(name: string): object {
-    return { name: name };
-  }
-
-  abstract describe(this: Department): void;
-
-  addEmployee(worker: string) {
-    this.workers.push(worker);
-  }
-
-  printWorkerInformation() {
-    console.log(this.workers.length);
-    console.log(this.workers);
-  }
+  greet(phrase: string): void;
 }
 
-// const marketing = new Department(4, 'marketing');
+class Person implements Greetable {
+  _name: string;
+  age = 34;
 
-// console.log(marketing.describe());
-
-// marketing.addEmployee('Bob');
-// marketing.addEmployee('Hal');
-// marketing.addEmployee('Kal');
-// marketing.printWorkerInformation();
-
-class ITDepartment extends Department {
-  constructor(id: number, public admins: string[]) {
-    super(id, 'IT');
+  constructor(n: string) {
+    this._name = n;
   }
 
-  describe(this: ITDepartment) {
-    return `Tech department is: ${this.name} and id ${this.id.toString()}`;
+  public get name(): string {
+    return this._name;
   }
+
+  public set name(value: string) {
+    this._name = `${value} ... `;
+  }
+
+  greet = (phrase: string) => {
+    console.log(`${this.name} says, ${phrase}`);
+  };
 }
 
-const IT = new ITDepartment(2, ['Kal']);
-console.log(IT.describe());
+// let userOne: Greetable;
 
-console.log(Department.fiscalYear);
+let userOne = new Person('Bob');
+userOne.name = 'Kalvin';
 
-class Cleaning extends Department {
-  constructor(id: number) {
-    super(id, 'Cleaning');
-  }
-  describe(this: Cleaning) {
-    console.log('The Cleaning department');
-  }
-}
-
-abstract class Building {
-  constructor(protected location: string) {}
-}
-
-class HRBuilding extends Building {
-  private static instance: HRBuilding;
-
-  private constructor(location: string) {
-    super(location);
-  }
-
-  static getInstance() {
-    if (HRBuilding.instance) {
-      return this.instance;
-    }
-    this.instance = new HRBuilding('Main Street');
-    return this.instance;
-  }
-}
-
-const HR = HRBuilding.getInstance();
-console.log(HR);
-
-class Team {
-  private _teamName: string;
-
-  constructor() {
-    this._teamName = '';
-  }
-
-  get teamName(): string {
-    return this._teamName;
-  }
-
-  set teamName(value: string) {
-    this._teamName = value;
-  }
-}
+userOne.greet('hello there...');
